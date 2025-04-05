734302f94f6e25efcbea547a4ad94d3b6081f499
Chanok15 <xtiansuva15@gmail.com>
2025-04-05 16:33:29 +0800

    edited codes by christian

--- lib/main.dart
+++ lib/main.dart
@@ -2,147 +2,237 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:intl/intl.dart';
 
 
-void main () async{
+void main() async {
   await Hive.initFlutter();
-  var box =await Hive.openBox('test');
-
-
+  await Hive.openBox('notesBox');
 
   runApp(CupertinoApp(
     debugShowCheckedModeBanner: false,
-
-    home: MyApp(),));
+    home: NotesApp(),
+  ));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class NotesApp extends StatefulWidget {
+  const NotesApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<NotesApp> createState() => _NotesAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  List <dynamic> todoList = [];
-  TextEditingController _addTask = TextEditingController();
-  var box = Hive.box('test');
+class _NotesAppState extends State<NotesApp> {
+  List<Map<String, dynamic>> notesList = [];
+  TextEditingController _addNoteTitle = TextEditingController();
+  TextEditingController _addNoteContent = TextEditingController();
+  TextEditingController _searchController = TextEditingController();
+  var notesBox = Hive.box('notesBox');
+  bool _showPinned = true;
+
   @override
   void initState() {
-
-    try {
-      todoList = box.get('todo');
-      print(todoList);
-    }catch (e) {
-      todoList = [];
-    }
     super.initState();
+    _loadNotes();
   }
+
+  void _loadNotes() {
+    try {
+      var storedNotes = notesBox.get('notes', defaultValue: []);
+
+      if (storedNotes is List) {
+        notesList = storedNotes.map((item) {
+          if (item is Map) {
+            return item.cast<String, dynamic>();
+          } else {
+            return <String, dynamic>{};
+          }
+        }).toList();
+      } else {
+        notesList = [];
+      }
+    } catch (e) {
+      notesList = [];
+      print('Error loading notes: $e');
+    }
+  }
+
+  void _saveNotes() {
+    List<Map<String, dynamic>> typedNotes = notesList.map((note) {
+      return note.cast<String, dynamic>();
+    }).toList();
+
+    notesBox.put('notes', typedNotes);
+  }
+
+  List<Map<String, dynamic>> _getFilteredNotes() {
+    final searchQuery = _searchController.text.toLowerCase();
+    if (searchQuery.isEmpty) {
+      return notesList;
+    } else {
+      return notesList.where((note) {
+        final title = note['title']?.toLowerCase() ?? '';
+        final content = note['content']?.toLowerCase() ?? '';
+        return title.contains(searchQuery) || content.contains(searchQuery);
+      }).toList();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          middle: Text ('Task', style: TextStyle(color: CupertinoColors.systemYellow),)
+      navigationBar: CupertinoNavigationBar(
+        middle: Text(
+          'Task',
+          style: TextStyle(color: CupertinoColors.systemYellow),
         ),
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-               children: [
-          Row(
-            children: [
-              Text('ToDo', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30)),
-            ],
-          ),
-
-                 Expanded(child: ListView.builder(
-                     itemCount: todoList.length,
-                     itemBuilder: (context, int index){
-                       final item= todoList;
-                      return GestureDetector(
-                        onLongPress: (){
-                          showCupertinoDialog(context: context, builder: (context){
-                            return CupertinoAlertDialog(
-                              title: Text('Delete'),
-                              content: Text ('Remove ${item[index]['task']} ?'),
-                              actions: [
-                                CupertinoButton(child: Text ('Yes', style: TextStyle(color: CupertinoColors.destructiveRed),), onPressed: (){
-                                    setState(() {
-                                      item.removeAt(index);
-                                      box.put('todo', item);
-                                    });
-                                  Navigator.pop(context);
-                                }),
-      CupertinoButton(child: Text ('No'), onPressed: (){
-
-                                  Navigator.pop(context);
-                                }),
-                              ],
-                            );
-                          });
-                        },
-                        onTap: (){
-
-                         setState(() {
-                           item[index]['status'] = !item[index]['status'];
-                            box.put('todo', item);
-                         });
-
-                        },
-                        child: Container(
-                          child: CupertinoListTile(
-                              title: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(item[index]['task'], style: TextStyle(decoration: item[index]['status']? TextDecoration.lineThrough : null),),
-                                  Icon(CupertinoIcons.circle_fill, size: 15, color: item[index]['status']?CupertinoColors.activeGreen: CupertinoColors.destructiveRed,)
-                                ],
-                              ),
-                            subtitle: Divider(color: CupertinoColors.systemFill.withOpacity(0.5)),
-                          ),
-                        ),
-                      );
-                 }) ),
-
-                 Container(
-                   color: CupertinoColors.systemFill.withOpacity(0.1),
-                   child: Row (
-                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                     children: [
-                       Text('               '),
-                       Text('${box.get('todo').length}ToDo'),
-                       CupertinoButton(child: Icon(CupertinoIcons.square_pencil, color: CupertinoColors.systemYellow,), onPressed: () {
-                         showCupertinoDialog(context: context, builder: (context){
+      ),
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Text('ToDo',
+                    style:
+                        TextStyle(fontWeight: FontWeight.w700, fontSize: 30)),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: todoList.length,
+                itemBuilder: (context, int index) {
+                  final item = todoList;
+                  return GestureDetector(
+                    onLongPress: () {
+                      showCupertinoDialog(
+                        context: context,
+                        builder: (context) {
                           return CupertinoAlertDialog(
-                          title: Text('Add Task'),
+                            title: Text('Delete'),
+                            content: Text('Remove ${item[index]['task']} ?'),
+                            actions: [
+                              CupertinoButton(
+                                child: Text(
+                                  'Yes',
+                                  style: TextStyle(
+                                      color: CupertinoColors.destructiveRed),
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    item.removeAt(index);
+                                    box.put('todo', item);
+                                  });
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              CupertinoButton(
+                                child: Text('No'),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    onTap: () {
+                      setState(() {
+                        item[index]['status'] = !item[index]['status'];
+                        box.put('todo', item);
+                      });
+                    },
+                    child: Container(
+                      child: CupertinoListTile(
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              item[index]['task'],
+                              style: TextStyle(
+                                  decoration: item[index]['status']
+                                      ? TextDecoration.lineThrough
+                                      : null),
+                            ),
+                            Icon(
+                              CupertinoIcons.circle_fill,
+                              size: 15,
+                              color: item[index]['status']
+                                  ? CupertinoColors.activeGreen
+                                  : CupertinoColors.destructiveRed,
+                            )
+                          ],
+                        ),
+                        subtitle: Divider(
+                            color: CupertinoColors.systemFill.withOpacity(0.5)),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Container(
+              color: CupertinoColors.systemFill.withOpacity(0.1),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('               '),
+                  Text(box.get('todo') != null
+                      ? '${box.get('todo').length}ToDo'
+                      : '0 ToDo'),
+                  CupertinoButton(
+                    child: Icon(
+                      CupertinoIcons.square_pencil,
+                      color: CupertinoColors.systemYellow,
+                    ),
+                    onPressed: () {
+                      showCupertinoDialog(
+                        context: context,
+                        builder: (context) {
+                          return CupertinoAlertDialog(
+                            title: Text('Add Task'),
                             content: CupertinoTextField(
                               placeholder: 'Add To-Do',
                               controller: _addTask,
                             ),
                             actions: [
-                              CupertinoButton(child: Text ('Close', style: TextStyle(color: CupertinoColors.destructiveRed),), onPressed: (){
-                                _addTask.text = "";
-                                Navigator.pop(context);
-                              }),
-                              CupertinoButton(child: Text ('Save'), onPressed: (){
-                               setState(() {
-                                     todoList.add({
-                                       "task" : _addTask.text,
-                                       "status" : false
-                                     });
-                                     box.put('todo', todoList);
-                                   });
-                                _addTask.text = "";
-                                Navigator.pop(context);
-                              }),
+                              CupertinoButton(
+                                child: Text('Close',
+                                    style: TextStyle(
+                                        color: CupertinoColors.destructiveRed)),
+                                onPressed: () {
+                                  _addTask.text = "";
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              CupertinoButton(
+                                child: Text('Save'),
+                                onPressed: () {
+                                  setState(() {
+                                    todoList.add({
+                                      "task": _addTask.text,
+                                      "status": false,
+                                    });
+                                    box.put('todo', todoList);
+                                    _loadTodoList(); // Reload the list after adding
+                                  });
+                                  _addTask.text = "";
+                                  Navigator.pop(context);
+                                },
+                              ),
                             ],
                           );
-                         });
-                       })
-                     ],
-                   ),
-                 )
-               ],
+                        },
+                      );
+                    },
+                  ),
+                ],
               ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
