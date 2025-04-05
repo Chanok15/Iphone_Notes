a33fc3df99e6e1572e800defb4fea9285552dcc5
Chanok15 <xtiansuva15@gmail.com>
2025-04-05 15:05:22 +0800

    commit by christian

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
-  // Your code here. For example:
-  runApp(MyApp());
+
+void main () async{
+  await Hive.initFlutter();
+  var box =await Hive.openBox('test');
+
+
+
+  runApp(CupertinoApp(
+    debugShowCheckedModeBanner: false,
+
+    home: MyApp(),));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List <dynamic> todoList = [];
+  TextEditingController _addTask = TextEditingController();
+  var box = Hive.box('test');
+  @override
+  void initState() {
+
+    try {
+      todoList = box.get('todo');
+      print(todoList);
+    }catch (e) {
+      todoList = [];
+    }
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
