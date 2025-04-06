2f9dba1ef6696a822ec928b1ea5515a2090b7813
renceestoquee <Dustinaron0@gmail.com>
2025-04-06 22:19:22 +0800

    commit by rence

--- lib/edit_note_page.dart
+++ lib/edit_note_page.dart
@@ -27,3 +27,67 @@ class _EditNotePageState extends State<EditNotePage> {
     _titleController = TextEditingController(text: widget.note['title'] ?? '');
     _contentController = TextEditingController(text: widget.note['content'] ?? '');
   }
+
+   @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Edit Note'),
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          child: Text('Save'),
+          onPressed: () {
+            final updatedNote = {
+              'title': _titleController.text,
+              'content': _contentController.text,
+              'date': DateTime.now(),
+              'isPinned': widget.note['isPinned'],
+              'isLocked': widget.note['isLocked'],
+            };
+            widget.onNoteUpdated(updatedNote);
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // Make the column take only necessary space
+            children: [
+              CupertinoTextField(
+                controller: _titleController,
+                placeholder: 'Title',
+              ),
+              SizedBox(height: 8),
+              Container(
+                constraints: BoxConstraints(maxHeight: 100.0), // Reduced height
+                child: CupertinoTextField(
+                  controller: _contentController,
+                  placeholder: 'Content',
+                  maxLines: null,
+                  expands: true,
+                  textAlignVertical: TextAlignVertical.top,
+                ),
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(DateFormat('MMM d, HH:mm').format(widget.note['date'])),
+                  CupertinoButton(
+                    padding: EdgeInsets.zero,
+                    child: Text('Cancel'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
